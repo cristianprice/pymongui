0d113d290160dc1f6148950b8254614b181ed327
Priceputu Cristian <cristian.priceputu@woowteam.com>
2019-01-28 13:27:02 +0200

    Initial files.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='monetui',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Priceputu Cristian',
+    author_email='priceputu.cristian@gmail.com',
+    description='',
+    install_requires=['pymonetdb', 'better_exceptions']
+)
