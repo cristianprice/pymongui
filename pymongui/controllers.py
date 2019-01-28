6634e0f53358ee7b018963afa7831c56e6184a80
Priceputu Cristian <cristian.priceputu@woowteam.com>
2019-01-28 16:18:24 +0200

    Added enum functions to tables.

--- pymongui/controllers.py
+++ pymongui/controllers.py
@@ -3,6 +3,9 @@ from os import cpu_count
 
 from pymongui.logging_wrapper import logger
 from pymongui.monet_db import create_db_connection
+from pymongui.monet_db import enum_schemas
+from pymongui.monet_db import enum_tables
+from pymongui.monet_db import enum_columns
 
 
 def task_execution_context(thread_count=cpu_count()):
@@ -14,5 +17,9 @@ def task_execution_context(thread_count=cpu_count()):
 
 if __name__ == '__main__':
     ctx = task_execution_context()
-    with create_db_connection() as conn:
-        print(conn)
+    with create_db_connection(database='events') as conn:
+
+        for s in enum_schemas(conn):
+            for t in enum_tables(conn, s):
+                for c in enum_columns(conn, t):
+                    print(c)
--- pymongui/monet_db.py
+++ pymongui/monet_db.py
@@ -1,6 +1,11 @@
 import pymonetdb
 from pymongui.logging_wrapper import logger
 from contextlib import contextmanager
+from collections import namedtuple
+
+Schema = namedtuple('Schema', ['id', 'name', 'authorization', 'owner', 'system'])
+Table = namedtuple('Table', ['id', 'name', 'schema_id', 'query', 'type', 'system', 'commit_action', 'access', 'temporary'])
+Column = namedtuple('Column', ['id', 'name', 'type', 'type_digits', 'type_scale', 'table_id', 'default', 'null', 'number', 'storage'])
 
 
 @contextmanager
@@ -12,3 +17,49 @@ def create_db_connection(username="monetdb", password="monetdb", hostname="local
 
     yield connection
     connection.close()
+
+
+def enum_schemas(conn):
+    '''
+    Gets all schemas in the server.
+    :param conn: the db connection.
+    :return: an iterator of rows with details.
+    '''
+
+    cur = conn.cursor()
+    cur.execute('select * from sys.schemas order by name asc')
+
+    for row in cur:
+        yield Schema(*row)
+
+
+def enum_tables(conn, schema_details):
+    '''
+    Returns tables in a schema.
+    :param conn: the db connection.
+    :param schema_details: the schema details.
+    :return:
+    '''
+
+    logger().info('Getting tables in schema: %s', schema_details.name)
+    cur = conn.cursor()
+    cur.execute('select * from sys.tables where schema_id = %s order by name asc', (schema_details.id,))
+
+    for row in cur:
+        yield Table(*row)
+
+
+def enum_columns(conn, table_details):
+    '''
+
+    :param conn: the db connection.
+    :param table_id: the table details.
+    :return:
+    '''
+
+    logger().info('Getting columns in table: %s', table_details.name)
+    cur = conn.cursor()
+    cur.execute('select * from sys.columns where table_id = %s order by name asc', (table_details.id,))
+
+    for row in cur:
+        yield Column(*row)
